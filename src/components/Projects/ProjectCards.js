9782b49bea1d94f81c1d056df7f5c8428d193ae1
andesson reis <andessonreys@gmail.com>
2023-12-10 23:05:20 -0300

    styles: comentarios temporais

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -20,7 +21,7 @@ function ProjectCards(props) {
         {"\n"}
         {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+        {// If the component contains Demo link and if it's not a Blog then, it will render the below component  }
 
         {!props.isBlog && props.demoLink && (
           <Button
@@ -38,3 +39,4 @@ function ProjectCards(props) {
   );
 }
 export default ProjectCards;
+*/
