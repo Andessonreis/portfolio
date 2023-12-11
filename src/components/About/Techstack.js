0a80f7aaebc6e1ca90713c3891ceed546d1d7edb
andesson reis <andessonreys@gmail.com>
2023-12-11 00:52:53 -0300

    fix: resolvido erro com os imports

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,56 +1,69 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiPython,
-  SiSqlite,
-  SiApachespark,
-  SiPandas,
-  SiNumpy,
-  SiJava,
-  SiSpring,
-  SiBootstrap,
-  SiJquery,
-  SiJavascript,
-  SiHtml5,
-  SiCss3,
-  SiPytorch,
-  SiTidyverse,
-  SiC,
-  Sicplusplus,
-  SiGolang,
+  DiJava,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJavascript1,
+  DiCode,
+  DiBootstrap,
+  DiJqueryLogo,
+  DiPostgresql,
+  DiCss3,
+  DiHtml5,
+} from "react-icons/di";
+import {
+  SiRedis,
+  SiFirebase,
+  SiNextdotjs,
+  SiSolidity,
 } from "react-icons/si";
-import { FaDatabase } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
 
 function Techstack() {
-  const techIcons = [
-    <SiPython />,
-    <SiSqlite />,
-    <SiApachespark />,
-    <SiPandas />,
-    <SiNumpy />,
-    <SiJava />,
-    <SiSpring />,
-    <SiBootstrap />,
-    <SiJquery />,
-    <SiJavascript />,
-    <SiHtml5 />,
-    <SiCss3 />,
-    <SiPytorch />,
-    <SiTidyverse />,
-    <SiC />,
-    <Sicplusplus />,
-    <SiGolang />,
-  ];
-
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {techIcons.map((icon, index) => (
-        <Col key={index} xs={4} md={2} className="tech-icons">
-          {icon}
-        </Col>
-      ))}
       <Col xs={4} md={2} className="tech-icons">
-        <FaDatabase />
+        <DiCode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSolidity />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaJava />
       </Col>
     </Row>
   );
