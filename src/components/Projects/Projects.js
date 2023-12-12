f12ea591b5834d0bc3740e74bd77b0a744430833
andesson reis <andessonreys@gmail.com>
2023-12-12 15:20:30 -0300

    feat: Adicionando SGIF

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,15 +1,16 @@
-/*
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
+
+import sgif from "../../Assets/Projects/sgif.png";
+/*
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
-
+*/
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -24,15 +25,14 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={sgif}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="SGIF"
+              description="System of management for ifba in java and Spring"
+              ghLink="https://github.com/jonatasfbastos/sgif_backend"
             />
           </Col>
-
+         /*
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bitsOfCode}
@@ -88,6 +88,8 @@ function Projects() {
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
+          */
+
         </Row>
       </Container>
     </Container>
@@ -95,4 +97,3 @@ function Projects() {
 }
 
 export default Projects;
-*/
